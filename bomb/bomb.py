c386b15d91ce737c2e8f5d5d185ef0ca9f4104da
victor-home-pc <volsavaivictor@gmail.com>
2024-03-16 02:53:13 +1000

    Update: Bomb Mods

--- bomb/bomb.py
+++ bomb/bomb.py
@@ -7,6 +7,9 @@ class Pile:
     
     def get(self) -> list:
         return self._pile
+    
+    def init(self, pile:list=[]) -> None:
+        self._pile = pile
 
     def add(self, cards:list) -> None:      #? Add Element(s) to End of 
         for card in cards:
@@ -23,19 +26,29 @@ class Pile:
 
 
 
-class Bomb(Pile):
+class Bomb():
     def __init__(self) -> None:
         self._deck = Pile()
         self._board = Pile()
 
         self.setDeck()
     
-    def setDeck(self) -> None:      #? Set Deck of Cards Elements to Deck
-        self._deck
-        self._deck.add([j+i for i in 'DSHC' for j in 'A23456789TJQK'])
+    def getDeck(self) -> list:
+        return self._deck
     
+    def getBoard(self) -> list:
+        return self._board
+
+    def setDeck(self) -> None:      #? Set Deck of Cards Elements to Deck
+        self._deck.init([j+i for i in 'DSHC' for j in 'A23456789TJQK'])
+
     def draw(self, count:int=1) -> list:        #? Remove a number of Cards from the Deck
         return self._deck.rmv(count=count)
     
     def discard(self, cards:list) -> None:      #? Add Cards discarded from Hand to the Board
         self._board.add(cards)
+
+    def repileDeck(self) -> None:
+        if len(self._deck) == 1:
+            self._deck.add(self._board)
+            self._board.init()
