5d29945ffa2e854984144e7e6670f39c264c54d8
Victor <volsavaivictor@gmail.com>
2024-03-15 16:24:06 +1000

    Alter: Bomb Mod - Pile Class

--- bomb/bomb.py
+++ bomb/bomb.py
@@ -1,14 +1,26 @@
-class Deck:
-    def __init__(self):
-        self.deck_id = ''
-        self.deck_cards = 0
-
-    def getDeck(self):
-        pass
-
-class Player:
-    def __init__(self, id, uname):
-        pass
+import random
 
 class Pile:
-    pass
+    #? CONSTRUCTOR
+    def __init__(self) -> None:
+        self._pile = []
+    
+
+    def get(self) -> list:
+        return self._pile
+
+
+    def add(self, cards:list) -> None:
+        for card in cards:
+            self._pile.append(card)
+
+
+    def rmv(self, pos:str='top', count:int=1) -> list:
+        if pos=='btm':
+            return [self._pile.pop() for i in range(count)]
+        else:
+            return [self._pile.pop(0) for i in range(count)]
+        
+
+    def shuffle(self) -> None:
+        random.shuffle(self._pile)
